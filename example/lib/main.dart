cae6aa101d4646a0efd9407a1694078bc40a5cd3
r <nsyiqnakfgxizos@mailo.com>
2023-06-23 23:30:45 +0100

    Upgrade to Flutter SDK to '>=3.0.1 <4.0.0'; upgrade intl to ^0.18.0

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,9 @@ class MyApp extends StatelessWidget {
         GlobalMaterialLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: [Locale('pt', 'BR')],
+      supportedLocales: const [
+        Locale('en', ''),
+      ],
     );
   }
 }
@@ -91,145 +93,164 @@ class _MyHomePageState extends State<MyHomePage> {
           key: _oFormKey,
           child: Column(
             children: <Widget>[
+              // start mine
               DateTimePicker(
-                type: DateTimePickerType.dateTimeSeparate,
-                dateMask: 'd MMM, yyyy',
-                controller: _controller1,
-                //initialValue: _initialValue,
-                firstDate: DateTime(2000),
-                lastDate: DateTime(2100),
-                icon: Icon(Icons.event),
-                dateLabelText: 'Date',
-                timeLabelText: "Hour",
-                //use24HourFormat: false,
-                //locale: Locale('pt', 'BR'),
-                selectableDayPredicate: (date) {
-                  if (date.weekday == 6 || date.weekday == 7) {
-                    return false;
-                  }
-                  return true;
-                },
-                onChanged: (val) => setState(() => _valueChanged1 = val),
-                validator: (val) {
-                  setState(() => _valueToValidate1 = val ?? '');
-                  return null;
-                },
-                onSaved: (val) => setState(() => _valueSaved1 = val ?? ''),
-              ),
-              DateTimePicker(
-                type: DateTimePickerType.dateTime,
-                dateMask: 'd MMMM, yyyy - hh:mm a',
-                controller: _controller2,
-                //initialValue: _initialValue,
-                firstDate: DateTime(2000),
-                lastDate: DateTime(2100),
-                //icon: Icon(Icons.event),
-                dateLabelText: 'Date Time',
                 use24HourFormat: false,
-                locale: Locale('en', 'US'),
-                onChanged: (val) => setState(() => _valueChanged2 = val),
-                validator: (val) {
-                  setState(() => _valueToValidate2 = val ?? '');
-                  return null;
-                },
-                onSaved: (val) => setState(() => _valueSaved2 = val ?? ''),
+                locale: Localizations.localeOf(context),
+                type: DateTimePickerType.dateTime,
+                dateMask: 'd MMM, yyyy - hh:mm a',
+                initialValue:
+                    DateTime.now().subtract(Duration(days: 1)).toString(),
+                firstDate: DateTime.now().subtract(
+                  const Duration(days: 365 * 10),
+                ),
+                lastDate: DateTime.now(),
+                onChanged: null,
+                style: const TextStyle(fontSize: 18, color: Colors.black),
               ),
-              DateTimePicker(
-                type: DateTimePickerType.date,
-                dateMask: 'yyyy/MM/dd',
-                controller: _controller3,
-                //initialValue: _initialValue,
-                firstDate: DateTime(2000),
-                lastDate: DateTime(2100),
-                icon: Icon(Icons.event),
-                dateLabelText: 'Date',
-                //locale: Locale('en', 'US'),
-                onChanged: (val) => setState(() => _valueChanged3 = val),
-                validator: (val) {
-                  setState(() => _valueToValidate3 = val ?? '');
-                  return null;
-                },
-                onSaved: (val) => setState(() => _valueSaved3 = val ?? ''),
-              ),
-              DateTimePicker(
-                type: DateTimePickerType.time,
-                controller: _controller4,
-                //initialValue: _initialValue,
-                icon: Icon(Icons.access_time),
-                timeLabelText: "Time",
-                //use24HourFormat: false,
-                //locale: Locale('en', 'US'),
-                onChanged: (val) => setState(() => _valueChanged4 = val),
-                validator: (val) {
-                  setState(() => _valueToValidate4 = val ?? '');
-                  return null;
-                },
-                onSaved: (val) => setState(() => _valueSaved4 = val ?? ''),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'DateTimePicker data value onChanged:',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              SelectableText(_valueChanged1),
-              SelectableText(_valueChanged2),
-              SelectableText(_valueChanged3),
-              SelectableText(_valueChanged4),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
-                  final loForm = _oFormKey.currentState;
+              // end mine
+              // DateTimePicker(
+              //   use24HourFormat: false,
+              //   type: DateTimePickerType.dateTimeSeparate,
+              //   dateMask: 'd MMM, yyyy',
+              //   controller: _controller1,
+              //   //initialValue: _initialValue,
+              //   firstDate: DateTime(2000),
+              //   lastDate: DateTime(2100),
+              //   icon: Icon(Icons.event),
+              //   dateLabelText: 'Date',
+              //   timeLabelText: "Hour",
+              //   //use24HourFormat: false,
+              //   //locale: Locale('pt', 'BR'),
+              //   selectableDayPredicate: (date) {
+              //     if (date.weekday == 6 || date.weekday == 7) {
+              //       return false;
+              //     }
+              //     return true;
+              //   },
+              //   onChanged: (val) => setState(() => _valueChanged1 = val),
+              //   validator: (val) {
+              //     setState(() => _valueToValidate1 = val ?? '');
+              //     return null;
+              //   },
+              //   onSaved: (val) => setState(() => _valueSaved1 = val ?? ''),
+              // ),
+              // DateTimePicker(
+              //   type: DateTimePickerType.dateTime,
+              //   dateMask: 'd MMMM, yyyy - hh:mm a',
+              //   controller: _controller2,
+              //   //initialValue: _initialValue,
+              //   firstDate: DateTime(2000),
+              //   lastDate: DateTime(2100),
+              //   //icon: Icon(Icons.event),
+              //   dateLabelText: 'Date Time',
+              //   use24HourFormat: false,
+              //   locale: Locale('en', 'US'),
+              //   onChanged: (val) => setState(() => _valueChanged2 = val),
+              //   validator: (val) {
+              //     setState(() => _valueToValidate2 = val ?? '');
+              //     return null;
+              //   },
+              //   onSaved: (val) => setState(() => _valueSaved2 = val ?? ''),
+              // ),
+              // DateTimePicker(
+              //   use24HourFormat: false,
+              //   type: DateTimePickerType.date,
+              //   dateMask: 'yyyy/MM/dd',
+              //   controller: _controller3,
+              //   //initialValue: _initialValue,
+              //   firstDate: DateTime(2000),
+              //   lastDate: DateTime(2100),
+              //   icon: Icon(Icons.event),
+              //   dateLabelText: 'Date',
+              //   //locale: Locale('en', 'US'),
+              //   onChanged: (val) => setState(() => _valueChanged3 = val),
+              //   validator: (val) {
+              //     setState(() => _valueToValidate3 = val ?? '');
+              //     return null;
+              //   },
+              //   onSaved: (val) => setState(() => _valueSaved3 = val ?? ''),
+              // ),
+              // DateTimePicker(
+              //   use24HourFormat: false,
+              //   type: DateTimePickerType.time,
+              //   controller: _controller4,
+              //   //initialValue: _initialValue,
+              //   icon: Icon(Icons.access_time),
+              //   timeLabelText: "Time",
+              //   //use24HourFormat: false,
+              //   //locale: Locale('en', 'US'),
+              //   onChanged: (val) => setState(() => _valueChanged4 = val),
+              //   validator: (val) {
+              //     setState(() => _valueToValidate4 = val ?? '');
+              //     return null;
+              //   },
+              //   onSaved: (val) => setState(() => _valueSaved4 = val ?? ''),
+              // ),
+              // SizedBox(height: 20),
+              // Text(
+              //   'DateTimePicker data value onChanged:',
+              //   style: TextStyle(fontWeight: FontWeight.bold),
+              // ),
+              // SizedBox(height: 10),
+              // SelectableText(_valueChanged1),
+              // SelectableText(_valueChanged2),
+              // SelectableText(_valueChanged3),
+              // SelectableText(_valueChanged4),
+              // SizedBox(height: 10),
+              // ElevatedButton(
+              //   onPressed: () {
+              //     final loForm = _oFormKey.currentState;
 
-                  if (loForm?.validate() == true) {
-                    loForm?.save();
-                  }
-                },
-                child: Text('Submit'),
-              ),
-              SizedBox(height: 30),
-              Text(
-                'DateTimePicker data value validator:',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              SelectableText(_valueToValidate1),
-              SelectableText(_valueToValidate2),
-              SelectableText(_valueToValidate3),
-              SelectableText(_valueToValidate4),
-              SizedBox(height: 10),
-              Text(
-                'DateTimePicker data value onSaved:',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              SelectableText(_valueSaved1),
-              SelectableText(_valueSaved2),
-              SelectableText(_valueSaved3),
-              SelectableText(_valueSaved4),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  final loForm = _oFormKey.currentState;
-                  loForm?.reset();
+              //     if (loForm?.validate() == true) {
+              //       loForm?.save();
+              //     }
+              //   },
+              //   child: Text('Submit'),
+              // ),
+              // SizedBox(height: 30),
+              // Text(
+              //   'DateTimePicker data value validator:',
+              //   style: TextStyle(fontWeight: FontWeight.bold),
+              // ),
+              // SizedBox(height: 10),
+              // SelectableText(_valueToValidate1),
+              // SelectableText(_valueToValidate2),
+              // SelectableText(_valueToValidate3),
+              // SelectableText(_valueToValidate4),
+              // SizedBox(height: 10),
+              // Text(
+              //   'DateTimePicker data value onSaved:',
+              //   style: TextStyle(fontWeight: FontWeight.bold),
+              // ),
+              // SizedBox(height: 10),
+              // SelectableText(_valueSaved1),
+              // SelectableText(_valueSaved2),
+              // SelectableText(_valueSaved3),
+              // SelectableText(_valueSaved4),
+              // SizedBox(height: 20),
+              // ElevatedButton(
+              //   onPressed: () {
+              //     final loForm = _oFormKey.currentState;
+              //     loForm?.reset();
 
-                  setState(() {
-                    _valueChanged1 = '';
-                    _valueChanged2 = '';
-                    _valueChanged3 = '';
-                    _valueChanged4 = '';
-                    _valueToValidate1 = '';
-                    _valueToValidate2 = '';
-                    _valueToValidate3 = '';
-                    _valueToValidate4 = '';
-                    _valueSaved1 = '';
-                    _valueSaved2 = '';
-                    _valueSaved3 = '';
-                    _valueSaved4 = '';
-                  });
-                },
-                child: Text('Reset'),
-              ),
+              //     setState(() {
+              //       _valueChanged1 = '';
+              //       _valueChanged2 = '';
+              //       _valueChanged3 = '';
+              //       _valueChanged4 = '';
+              //       _valueToValidate1 = '';
+              //       _valueToValidate2 = '';
+              //       _valueToValidate3 = '';
+              //       _valueToValidate4 = '';
+              //       _valueSaved1 = '';
+              //       _valueSaved2 = '';
+              //       _valueSaved3 = '';
+              //       _valueSaved4 = '';
+              //     });
+              //   },
+              //   child: Text('Reset'),
+              // ),
             ],
           ),
         ),
